243fb3936a983116490e21cd39b7d76cf0eb0c33
denwayde <denwayde09@gmail.com>
2024-04-24 13:34:29 +0500

    ehfeu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import './App.css';
-
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-
 import 'primeicons/primeicons.css';
-
 import 'primeflex/primeflex.css'
-
 import MyForm from './components/MyForm';
+import {useTelegram} from './hooks/useTelegram'
 import { useEffect } from 'react';
 
 
-const tg = window.Telegram.WebApp
+
 function App() {
-  
+  const {tg} = useTelegram()
   useEffect(()=>{
     tg.ready()
   }, [])
-
   return (
     <div className="App">
       <MyForm/>
