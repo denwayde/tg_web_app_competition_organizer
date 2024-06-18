a87ad13ec9a7ea0d49740683bda93ddfeac5bc23
den wayde <den.wayde@yandex.ru>
2024-06-18 13:41:44 +0500

    а3а3

--- src/store/nameInputReducer.js
+++ src/store/nameInputReducer.js
@@ -1,6 +1,14 @@
 const defaultState = {
     value: ''
 }
+export const SETVALUE = 'SETVALUE'
+
 export function nameReducer(state = defaultState, action){
-    
+    switch(action.type){
+        case SETVALUE:
+            return {...state, value: action.payload}
+    }
+    return state
 }
+
+
